473b34a249c2110483538084f29e55467e377bee
myrdall <trivarson@gmail.com>
2020-10-18 12:43:26 +0700

    Project Init

---
+++ Calculator/Calculator/RunCalcForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    static class RunCalcForm
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CalcForm());
+        }
+    }
+}
