cc4986bb6f8b2c14d0730926c89a46caf7a71e8e
myrdall <trivarson@gmail.com>
2020-10-18 14:56:51 +0700

    Second init unittestproject and add first test function

---
+++ Calculator/Calculator/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f34b85d7-3f2b-48a7-81de-0268610a5766")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator/Calculator/Test/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void FactorialTest()
+        {
+            //arrage
+
+            int x = 3;
+
+            int expected = 6;
+
+            //act
+
+            CalcFinction factorial = new CalcFinction();
+
+            double actual = factorial.Factorial(x);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
