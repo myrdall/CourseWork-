59af972e0d07af494ffaa3922eb343707995d999
myrdall <trivarson@gmail.com>
2020-10-18 15:25:44 +0700

    add more test function

--- Calculator/Calculator/Test/Test.cs
+++ Calculator/Calculator/Test/Test.cs
@@ -7,7 +7,7 @@ namespace Calculator
     public class Test
     {
         [TestMethod]
-        public void FactorialTest()
+        public void FactorialTest_x3_expected_6()
         {
             //arrage
 
@@ -24,6 +24,92 @@ namespace Calculator
             //assert
 
             Assert.AreEqual(expected, actual);
+
         }
+
+        [TestMethod]
+        public void SumTest_forINT_x10_y10_expected_20()
+        {
+            //arrage
+
+            int x = 10;
+            int y = 10;
+
+            int expected = 20;
+
+            //act
+
+            CalcFinction intSumm = new CalcFinction();
+
+            double actual = intSumm.Summ(x,y);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SumTest_forDouble_x2dot5_y2dot5_expected_5()
+        {
+            //arrage
+
+            double x = 2.5;
+            double y = 2.5;
+
+            int expected = 5;
+
+            //act
+
+            CalcFinction intSumm = new CalcFinction();
+
+            double actual = intSumm.Summ(x, y);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SubTest_forINT_x20_y10_expected_10()
+        {
+            //arrage
+
+            int x = 20;
+            int y = 10;
+
+            int expected = 10;
+
+            //act
+
+            CalcFinction intSub = new CalcFinction();
+
+            double actual = intSub.Sub(x, y);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+/*
+        [TestMethod]
+       *//* public void SubTest_forDouble_x10dot5_y2dot5_expected_8()
+        {
+            //arrage
+
+            double x = 10.5;
+            double y = 2.5;
+
+            int expected = 8;
+
+            //act
+
+            CalcFinction intSub = new CalcFinction();
+
+            double actual = intSub.Sub(x, y);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }*/
+
     }
 }
