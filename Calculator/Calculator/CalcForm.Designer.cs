5ec35c7c0902b226ca71ae74d241b4b2ef3c681e
myrdall <trivarson@gmail.com>
2020-10-18 13:47:24 +0700

    Init design

--- Calculator/Calculator/CalcForm.Designer.cs
+++ Calculator/Calculator/CalcForm.Designer.cs
@@ -28,21 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.summButton = new System.Windows.Forms.Button();
+            this.displayX = new System.Windows.Forms.TextBox();
+            this.displayY = new System.Windows.Forms.TextBox();
+            this.displayAnswer = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // summButton
+            // 
+            this.summButton.Location = new System.Drawing.Point(125, 188);
+            this.summButton.Name = "summButton";
+            this.summButton.Size = new System.Drawing.Size(80, 51);
+            this.summButton.TabIndex = 0;
+            this.summButton.Text = "+";
+            this.summButton.UseVisualStyleBackColor = true;
+            this.summButton.Click += new System.EventHandler(this.SummButton_Click);
+            // 
+            // displayX
+            // 
+            this.displayX.Location = new System.Drawing.Point(53, 55);
+            this.displayX.Multiline = true;
+            this.displayX.Name = "displayX";
+            this.displayX.Size = new System.Drawing.Size(223, 35);
+            this.displayX.TabIndex = 1;
+            // 
+            // displayY
+            // 
+            this.displayY.Location = new System.Drawing.Point(53, 113);
+            this.displayY.Multiline = true;
+            this.displayY.Name = "displayY";
+            this.displayY.Size = new System.Drawing.Size(223, 35);
+            this.displayY.TabIndex = 2;
+            // 
+            // displayAnswer
+            // 
+            this.displayAnswer.Location = new System.Drawing.Point(53, 264);
+            this.displayAnswer.Multiline = true;
+            this.displayAnswer.Name = "displayAnswer";
+            this.displayAnswer.Size = new System.Drawing.Size(223, 35);
+            this.displayAnswer.TabIndex = 3;
+            // 
+            // CalcForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 376);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(370, 376);
+            this.Controls.Add(this.displayAnswer);
+            this.Controls.Add(this.displayY);
+            this.Controls.Add(this.displayX);
+            this.Controls.Add(this.summButton);
+            this.Name = "CalcForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button summButton;
+        private System.Windows.Forms.TextBox displayX;
+        private System.Windows.Forms.TextBox displayY;
+        private System.Windows.Forms.TextBox displayAnswer;
     }
 }
 
--- Calculator/Calculator/CalcForm.cs
+++ Calculator/Calculator/CalcForm.cs
@@ -14,11 +14,24 @@ namespace Calculator
         public CalcForm()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void SummButton_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(displayX.Text);
+            double y = double.Parse(displayY.Text);
+
+            CalcFinction s = new CalcFinction();
+
+            double result = s.Summ(x, y);
+
+            displayAnswer.Text = result.ToString();
         }
     }
 }
