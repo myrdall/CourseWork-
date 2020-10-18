42498dd4620c1705a0200196fb219f8c706dd41b
myrdall <trivarson@gmail.com>
2020-10-18 13:37:38 +0700

    Add more functions

--- Calculator/Calculator/CalcFinction.cs
+++ Calculator/Calculator/CalcFinction.cs
@@ -5,11 +5,33 @@ using System.Text;
 
 namespace Calculator
 {
-   public class CalcFinction
+    public class CalcFinction
     {
-        public int Summ(int x, int y)
+        public double Summ(double x, double y)
         {
-            return x+y;
+            return x + y;
         }
+
+        public double Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public double Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public double Div (int x, int y)
+        {
+            return x / y; 
+        }
+
+        public double Exp (double x, int pow)
+        {
+            double exp = Math.Pow(x, pow);
+            return exp;
+        }
+
     }
 }
